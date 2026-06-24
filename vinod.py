d0c808c6e8e9b126c02379df94835544269137b1
Vinodsankarapu <100195036+Vinodsankarapu@users.noreply.github.com>
2026-06-24 09:21:36 +0530

    Add files via upload

---
+++ vinod.py
@@ -0,0 +1,55 @@
+"""python introduction (day1)
+procedural language
+
+---->This follow a step-step-by approach, where code is structure into procedure such as function or routines...
+
+
+eg ----> C
+
+OOP'S:
+----
+--->this is based on concepts of object and class..
+
+PYTHON:
+-------
+1.dynamically typed
+----> python  know the type of data we are passing to the variable....
+2.interperter
+----> python execute code line-by-line , if any error it will stop execution in that  and before will gives the output...
+3.high-level
+4.1991 Guide Van Rossum
+
+Why use python:
+1.easy syntax
+2.cross-platform (open source)
+3.wide application
+4.huge library support
+
+tokens:
+1.keywords----> these are keywords in python
+eg---> if, else, for, while
+2) identifiers---->name given to variables, function, class
+3) literals--->assigned the write side variables is known as literals
+4) operators---->+. -, *
+5) punctuators--->(), [], {}
+
+variable:
+----> variable is name used to store data in a memory
+rules:
+1)start with characters, and underscore.
+2)cannot start with special characters, numbers, special characters.
+
+comments:
+1)single line ---> #
+2)multi line----> ''' ''' """ """
+
+print(89 + 67)
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2)
+print(id(total_))"""
+
+
+
